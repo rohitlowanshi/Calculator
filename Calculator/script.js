2104bda6d695eb7175e79907730ca7772221c9e3
Rohit Lowanshi <121011914+rohitlowanshi@users.noreply.github.com>
2023-03-01 23:28:10 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,19 @@
+let string ="";
+let button = document.querySelectorAll('.button,.but,.equal,.op');
+Array.from(button).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'C'){
+            string = ""
+            document.querySelector('input').value = string;
+        }
+        else{
+      console.log(e.target)
+        string += e.target.innerHTML;
+        document.querySelector('input').value = string;
+        }
+    })
+})
